fdcc2bd0f6a43f16db152d77daa601ef95ca178d
종성 박 <tjd1707@naver.com>
2021-05-27 14:52:24 +0900

    bold italic line 첨자 추가

---
+++ H2O_odf/StyleDetail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace H2O_odf
+{
+    class StyleDetail
+    {
+        float layout_left = 2;
+        float layout_right = 2;
+        float layout_top = 2;
+        float layout_bottom = 2;
+
+        float page_left = 0;
+        float page_right = 0;
+        float page_text_indent = 0;
+        public string pagelayout()
+        {
+            string str = "<style:page-layout-properties fo:page-width=\"21.001cm\" fo:page-height=\"29.7cm\" style:num-format=\"1\" style:print-orientation=\"portrait\" " +
+                "fo:margin-top=\""+layout_top+ "cm\" fo:margin-bottom=\"" + layout_bottom + "cm\" fo:margin-left=\"" + layout_left + "cm\" fo:margin-right=\"" + layout_right + "cm\" style:writing-mode=\"lr-tb\" " +
+                "style:layout-grid-color=\"#c0c0c0\" style:layout-grid-lines=\"20\" style:layout-grid-base-height=\"0.706cm\" style:layout-grid-ruby-height=\"0.353cm\" style:layout-grid-mode=\"none\" style:layout-grid-ruby-below=\"false\" style:layout-grid-print=\"false\" style:layout-grid-display=\"false\" " +
+                "style:footnote-max-height=\"0cm\">";
+            return str;
+        }
+
+        public string pagemargin()
+        {
+            string str = "<style:paragraph-properties ";
+            if(page_left != 0 || page_right != 0 || page_text_indent != 0)
+            {
+                str = str + "<fo:margin-left=\"" + page_left + "cm\" fo:margin-right=\"" + page_right + "cm\" fo:text-indent=\"" + page_text_indent + "cm\"  style:auto-text-indent=\"false\"";
+            }
+            str = str + "style:text-autospace=\"none\"/>";
+            return str;
+        }
+    }
+}
--- H2O_odf/contentDetail.cs
+++ H2O_odf/contentDetail.cs
@@ -11,6 +11,12 @@ namespace H2O_odf
         float margin_left = 0;
         float margin_right = 0;
         float text_ident = 0;
+        bool bold = false;
+        bool italic = false;
+        bool line_through = false;
+        bool underline = false;
+        bool text_super = false;
+        bool text_sub = false;
 
         public string textStyle()
         {
@@ -21,8 +27,61 @@ namespace H2O_odf
                   + " fo:margin-left=\"" + margin_left + "cm\""
                   + " fo:margin-right=\"" + margin_right + "cm\""
                   + " fo:text-indent=\"" + text_ident + "cm\" style:auto-text-indent=\"false\"/>";
-            }                 
-            style = style + "<style:text-properties officeooo:rsid=\"0007e3a5\" officeooo:paragraph-rsid=\"000b59b8\"/></style:style>";
+            }
+
+            string rsid = " officeooo:rsid=\"0007e3a5\" officeooo:paragraph-rsid=\"0007e3a5\"";
+            string text_line_through = "";
+            string text_position = "";
+            string font_style = "";
+            string text_underline = "";
+            string font_weight = "";
+            string font_style_asian = "";
+            string font_weight_asian = "";
+            string font_style_complex = "";
+            string font_weight_complex = "";
+
+            if (line_through)
+            {
+                text_line_through = " style:text-line-through-style=\"solid\" style:text-line-through-type=\"single\"";
+            }
+
+            if (text_super)
+            {
+                text_position = " style:text-position=\"super 58%\"";
+            }
+            else if (text_sub)
+            {
+                text_position = " style:text-position=\"sub 58%\"";
+            }
+
+            if (underline)
+            {
+                text_underline = " style:text-underline-style=\"solid\" style:text-underline-width=\"auto\" style:text-underline-color=\"font-color\"";
+            }
+            if (bold)
+            {
+                font_weight = " fo:font-weight=\"bold\"";
+                font_weight_asian = " style:font-weight-asian=\"bold\"";
+                font_weight_complex = " style:font-weight-complex=\"bold\"";
+            }
+            if (italic)
+            {
+                font_style = " fo:font-style=\"italic\"";
+                font_style_asian = " style:font-style-asian=\"italic\"";
+                font_style_complex = " style:font-style-complex=\"italic\"";
+            }       
+            style = style + "<style:text-properties" + 
+                text_line_through + 
+                text_position + 
+                font_style + 
+                text_underline + 
+                font_weight + 
+                rsid + 
+                font_style_asian + 
+                font_weight_asian + 
+                font_style_complex + 
+                font_weight_complex + "/></style:style>";
+            
             return style;
         }
 
