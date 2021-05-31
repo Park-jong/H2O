33c6a878d380bb739ba5097a6a5c96c0248b6f12
종성 박 <tjd1707@naver.com>
2021-05-31 20:08:51 +0900

    파일 교체, odt생성, form일단 주석처리

---
+++ H2O_odf/FileManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+
+namespace H2O__
+{
+    class FileManager
+    {
+        public FileManager()
+        {
+            Initial_Folder();
+        }
+
+        private string appPath;
+        private string fileName;
+        private string loadName;
+
+        private void Initial_Folder()
+        {
+            appPath = @"C:\Users\park\source\repos\H2O_odf\H2O_odf";
+
+
+            //fileName = @"\TestODT";
+            //loadName = @"\data";
+
+            XmlManager xm = new XmlManager();
+
+            xm.CreateODT();
+            xm.Update_Bold();
+            xm.SaveODT(xm.root);
+
+            try
+            {
+                ZipFile.CreateFromDirectory(appPath + @"\New File", appPath + @"\New.odt");
+            }
+            catch (IOException)
+            {
+                File.Delete(appPath + @"\New.odt");
+                ZipFile.CreateFromDirectory(appPath + @"\New File", appPath + @"\New.odt");
+            }
+
+            //xm.Create_Document(appPath + loadName);
+
+            //xm.Update_Text("Test Test one two one two");
+
+            //xm.Save_Document(appPath + fileName);
+        }
+
+        private void Initial_XML()
+        {
+
+        }
+    }
+}
---
+++ H2O_odf/MainForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace H2O__
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_create = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Controls.Add(this.btn_create);
+            this.panel1.Name = "panel1";
+            // 
+            // btn_create
+            // 
+            resources.ApplyResources(this.btn_create, "btn_create");
+            this.btn_create.Name = "btn_create";
+            this.btn_create.UseVisualStyleBackColor = true;
+            this.btn_create.Click += new System.EventHandler(this.btn_create_Click);
+            // 
+            // MainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_create;
+    }
+}
+
