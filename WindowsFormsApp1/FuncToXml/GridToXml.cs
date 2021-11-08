fa07f45af212c345f54a2d52b5f40a9ebb8a70b2
chayou20 <chayou20@naver.com>
2021-11-08 21:50:38 +0900

    GridToXml 수정/ 작업범위 표74~표75 중 표 74는 속성데이터에대한길이라 반영하지않음 표75에서 속성~CellSpacing까지 json에서 데이터 가져오는 것만 작업함/기존 JsonToOdt에서 TextToXml.cs에서만 작업하던 것을 paragraph단위로 모듈별로 작업하기 위해 기존 TextToXml.cs에 있었던 for문을 JsonToOdt로 반영했고 Text작업부분과 Grid작업부분을 분리함/GridToXml에서도 데이터를 가져오는 부분과 Xml로 표현하는 부분을 일부분 분리했고 이후에도 반영할 예정임

--- WindowsFormsApp1/FuncToXml/GridToXml.cs
+++ WindowsFormsApp1/FuncToXml/GridToXml.cs
@@ -14,10 +14,29 @@ namespace WindowsFormsApp1.FuncToXml
 
         }
 
+        JToken jsonTable;
+        JToken jsonRowList;
+        JToken jsonHeader;
+
+        int property; //표75 속성
+        int rowCount; //표75 RowCount
+        int nCols; //표75 nCols
+        int cellSpacing; //표75 CellSpacing
+
+        int columnCount;
+
+        private void setData()
+        {
+            property = jsonTable["property"]["value"].Value<int>();
+            rowCount = jsonTable["rowCount"].Value<int>();
+            nCols = 0; //값 찾지 못함
+            cellSpacing = jsonTable["cellSpacing"].Value<int>();
+
+            columnCount = jsonTable["columnCount"].Value<int>();
+        }
         public void Run(XmlManager xm, JToken json, JToken docJson, bool zeroCheck)
         {
             //table있는지 체크
-
             bool hasTable = false;
             int hasTableControlNum;
             int controlListCount = 0;
@@ -33,7 +52,11 @@ namespace WindowsFormsApp1.FuncToXml
             {
                 try
                 {
-                    object existTable = json["controlList"][controlList]["table"].Value<object>();
+                    //현재 테이블 JToken Setting
+                    jsonTable = json["controlList"][controlList]["table"];
+                    jsonRowList = json["controlList"][controlList]["rowList"];
+                    jsonHeader = json["controlList"][controlList]["header"];
+
                     hasTable = true;
                     hasTableControlNum = controlList;
                 }
@@ -44,16 +67,15 @@ namespace WindowsFormsApp1.FuncToXml
                 //table이 존재할때만 실행
                 if (hasTable)
                 {
+                    setData(); //table 관련 data Setting
 
-                    int rowCount = json["controlList"][controlList]["table"]["rowCount"].Value<int>();
-                    int columnCount = json["controlList"][controlList]["table"]["columnCount"].Value<int>();
-                    double tableMatginTop = Math.Round(json["controlList"][controlList]["table"]["topInnerMargin"].Value<int>() * 0.01 * 0.0352778, 3);
-                    double tableMatginBottom = Math.Round(json["controlList"][controlList]["table"]["bottomInnerMargin"].Value<int>() * 0.01 * 0.0352778, 3);
-                    double tableMatginLeft = Math.Round(json["controlList"][controlList]["table"]["leftInnerMargin"].Value<int>() * 0.01 * 0.0352778, 3);
-                    double tableMatginRight = Math.Round(json["controlList"][controlList]["table"]["rightInnerMargin"].Value<int>() * 0.01 * 0.0352778, 3); ;
+                    double tableMatginTop = Math.Round(jsonTable["topInnerMargin"].Value<int>() * 0.01 * 0.0352778, 3);
+                    double tableMatginBottom = Math.Round(jsonTable["bottomInnerMargin"].Value<int>() * 0.01 * 0.0352778, 3);
+                    double tableMatginLeft = Math.Round(jsonTable["leftInnerMargin"].Value<int>() * 0.01 * 0.0352778, 3);
+                    double tableMatginRight = Math.Round(jsonTable["rightInnerMargin"].Value<int>() * 0.01 * 0.0352778, 3); ;
 
                     string table = xm.MakeTable(rowCount, columnCount);
-                    xm.setTable(table, Math.Round(json["controlList"][controlList]["header"]["width"].Value<int>() * 0.01 * 0.0352778, 3));
+                    xm.setTable(table, Math.Round(jsonHeader["width"].Value<int>() * 0.01 * 0.0352778, 3));
                     //for(int c = 0; c < columnCount; c++)
                     //{
                     //    int colWidth = json["bodyText"]["sectionList"][s]["paragraphList"][i]["controlList"][controlList]["rowList"][0]["cellList"][c]["listHeader"]["width"].Value<int>();
@@ -64,20 +86,20 @@ namespace WindowsFormsApp1.FuncToXml
                     //    int rowHeight = json["bodyText"]["sectionList"][s]["paragraphList"][i]["controlList"][controlList]["rowList"][c]["cellList"][0]["listHeader"]["height"].Value<int>();
                     //    xm.setRow(table, c, Math.Round(rowHeight * 0.01 * 0.0352778, 3));
                     //}
-                    for (int rowIndex = 0; rowIndex < json["controlList"][controlList]["rowList"].Count(); rowIndex++)
+                    for (int rowIndex = 0; rowIndex < jsonRowList.Count(); rowIndex++)
                     {
-                        for (int colIndex = 0; colIndex < json["controlList"][controlList]["rowList"][rowIndex]["cellList"].Count(); colIndex++)
+                        for (int colIndex = 0; colIndex < jsonRowList[rowIndex]["cellList"].Count(); colIndex++)
                         {
-                            int rowNum = json["controlList"][controlList]["rowList"].Count();
-                            int colNum = json["controlList"][controlList]["rowList"][rowIndex]["cellList"].Count();
-                            int cellWidth = json["controlList"][controlList]["rowList"][rowIndex]["cellList"][colIndex]["listHeader"]["width"].Value<int>();
-                            int cellHeight = json["controlList"][controlList]["rowList"][rowIndex]["cellList"][colIndex]["listHeader"]["height"].Value<int>();
-                            int column_index = json["controlList"][controlList]["rowList"][rowIndex]["cellList"][colIndex]["listHeader"]["colIndex"].Value<int>();
-                            int row_index = json["controlList"][controlList]["rowList"][rowIndex]["cellList"][colIndex]["listHeader"]["rowIndex"].Value<int>();
-                            int margin_top = json["controlList"][controlList]["rowList"][rowIndex]["cellList"][colIndex]["listHeader"]["topMargin"].Value<int>();
-                            int margin_bottom = json["controlList"][controlList]["rowList"][rowIndex]["cellList"][colIndex]["listHeader"]["bottomMargin"].Value<int>();
-                            int margin_left = json["controlList"][controlList]["rowList"][rowIndex]["cellList"][colIndex]["listHeader"]["leftMargin"].Value<int>();
-                            int margin_right = json["controlList"][controlList]["rowList"][rowIndex]["cellList"][colIndex]["listHeader"]["rightMargin"].Value<int>();
+                            int rowNum = jsonRowList.Count();
+                            int colNum = jsonRowList[rowIndex]["cellList"].Count();
+                            int cellWidth = jsonRowList[rowIndex]["cellList"][colIndex]["listHeader"]["width"].Value<int>();
+                            int cellHeight = jsonRowList[rowIndex]["cellList"][colIndex]["listHeader"]["height"].Value<int>();
+                            int column_index = jsonRowList[rowIndex]["cellList"][colIndex]["listHeader"]["colIndex"].Value<int>();
+                            int row_index = jsonRowList[rowIndex]["cellList"][colIndex]["listHeader"]["rowIndex"].Value<int>();
+                            int margin_top = jsonRowList[rowIndex]["cellList"][colIndex]["listHeader"]["topMargin"].Value<int>();
+                            int margin_bottom = jsonRowList[rowIndex]["cellList"][colIndex]["listHeader"]["bottomMargin"].Value<int>();
+                            int margin_left = jsonRowList[rowIndex]["cellList"][colIndex]["listHeader"]["leftMargin"].Value<int>();
+                            int margin_right = jsonRowList[rowIndex]["cellList"][colIndex]["listHeader"]["rightMargin"].Value<int>();
                             xm.setCol(table, colIndex, Math.Round(cellWidth * 0.01 * 0.0352778, 3));
                             xm.setRow(table, rowIndex, Math.Round(cellHeight * 0.01 * 0.0352778, 3));
                             xm.SetCell(table, colNum, rowNum, column_index, row_index, Math.Round(cellHeight * 0.01 * 0.0352778, 3), Math.Round(cellWidth * 0.01 * 0.0352778, 3), Math.Round(margin_top * 0.01 * 0.0352778, 3), Math.Round(margin_bottom * 0.01 * 0.0352778, 3), Math.Round(margin_left * 0.01 * 0.0352778, 3), Math.Round(margin_right * 0.01 * 0.0352778, 3));
